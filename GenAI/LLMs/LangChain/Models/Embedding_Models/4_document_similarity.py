2422aed72922afff36279ecafce2b5fd182b5a17
actionxbar-14 <ankitpaths144@gmail.com>
2026-09-01 21:51:21 +0530

    output parser done!

--- GenAI/LLMs/LangChain/Models/Embedding_Models/4_document_similarity.py
+++ GenAI/LLMs/LangChain/Models/Embedding_Models/4_document_similarity.py
@@ -1,37 +1,37 @@
 
 
 
-from sklearn.metrices.pairwise import cosine_similarity 
-import numpy as np 
+# from sklearn.metrices.pairwise import cosine_similarity 
+# import numpy as np 
 
-load_dotenv()
+# load_dotenv()
 
-embedding = OpenAIEmbeddings(model='BAAI/bge-small-en-v1.5' ,dimensions=300)
+# embedding = OpenAIEmbeddings(model='BAAI/bge-small-en-v1.5' ,dimensions=300)
 
-documents = [
-    'Delhi is the capital of India.',
-    'Machine learning helps computers learn from data.',
-    'The sun rises in the east.',
-    'Python is widely used for artificial intelligence.'
-]
-
-
-
-query = 'tell me about virat kohli'
-
-doc_embeddings = embedding.embed_documents(documents)
-query_embedding = embedding.embed_query(query)
-
-
-scores = cosine_similarity([query_embedding] ,doc_embeddings)[0]
-
-index , score = sorted(list(enumerate(scores))) ,key = lambda x: x[1][-1]
+# documents = [
+#     'Delhi is the capital of India.',
+#     'Machine learning helps computers learn from data.',
+#     'The sun rises in the east.',
+#     'Python is widely used for artificial intelligence.'
+# ]
 
 
 
-print(query)
-print(documents[index])
-print("similarity score is :" ,score)
+# query = 'tell me about virat kohli'
+
+# doc_embeddings = embedding.embed_documents(documents)
+# query_embedding = embedding.embed_query(query)
+
+
+# scores = cosine_similarity([query_embedding] ,doc_embeddings)[0]
+
+# index , score = sorted(list(enumerate(scores))) ,key = lambda x: x[1][-1]
+
+
+
+# print(query)
+# print(documents[index])
+# print("similarity score is :" ,score)
 
 
 
