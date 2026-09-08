726d320ec6950a85a25d9ed1ff0bfcf24871b2ba
actionxbar-14 <ankitpaths144@gmail.com>
2026-09-08 20:00:01 +0530

    Runnables continue!

---
+++ GenAI/LLMs/LangChain/Runnables/runnable_parallel.py
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel
+
+
+load_dotenv() 
+
+
+
+llm = HuggingFaceEndpoint(
+    repo_id='openai/gpt-oss-20b',
+    task = "text-generation"
+)
+model = ChatHuggingFace(llm=llm)
+
+
+prompt1 = PromptTemplate(
+    template='Generate a tweet about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a Linkdin text post about {topic}',
+    input_variables=['topic']
+)
+
+
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'tweet' : RunnableSequence(prompt1 , model , parser),
+    'linkdin' : RunnableSequence(prompt2 , model , parser)
+})
+
+
+
+result = parallel_chain.invoke({'topic' : 'AI'})
+
+print(result['tweet'])
+print(result['linkdin'])
+
---
+++ GenAI/LLMs/LangChain/Runnables/runnable_passthrough.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel , RunnablePassthrough
+
+
+load_dotenv()
+
+
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables= ['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+llm = HuggingFaceEndpoint(
+    repo_id='openai/gpt-oss-20b',
+    task = "text-generation"
+)
+model = ChatHuggingFace(llm=llm)
+
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt1 , model , parser)
+
+parallel_chain = RunnableParallel({
+    'joke'  : RunnablePassthrough(),
+    'explanation' : RunnableSequence(prompt1 , model , parser)
+})
+
+
+final_chain = RunnableSequence(joke_gen_chain , parallel_chain)
+
+result = final_chain.invoke({'topic' : 'cricket'})
+
+print(result)
+
